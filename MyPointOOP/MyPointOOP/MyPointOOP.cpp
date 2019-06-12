0b1738e702f74c75a746636f7db6cbd2d18bddd7
Ho Duc <hoduc1997@gmail.com>
2019-06-12 16:41:02 +0700

    +add Ex MyPoint

---
+++ MyPointOOP/MyPointOOP/MyPointOOP.cpp
@@ -0,0 +1,28 @@
+// MyPointOOP.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "MyPoint.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	MyPoint *p0 = new MyPoint();
+	MyPoint *p1 = new MyPoint();
+	p1->SetX(1);
+	p1->SetY(1);
+
+	cout << "p0";
+	p0->Display();
+
+	cout << "p1";
+	p1->Display();
+
+	cout << "Distance between current point and p1: " << p0->Distance(*p1) << endl;
+
+	system("pause");
+    return 0;
+}
+
